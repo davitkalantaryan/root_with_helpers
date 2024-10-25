85487ea0205d10875b89903ae75675b235e5b253
Davit Kalantaryan <davit.kalantaryan@desy.de>
2024-10-25 13:32:30 +0200

    added script to compile root for the version 5_20_00

---
+++ scripts/unix_compile_root_5_20_00.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+scriptFileFullPath=`readlink -f ${0}`
+scriptDirectory=`dirname ${scriptFileFullPath}`
+
+# some defs
+rootVersinName=v5-20-00
+#installDir=/afs/ifh.de/group/pitz/doocs/data/ers/sys/${lsbCode}/opt/root-${rootVersinName}
+installDir=${scriptDirectory}/../install/${rootVersinName}
+configuration=Debug
+
+${scriptDirectory}/unix_compile_root_args_version_installpath_conf.sh ${rootVersinName} ${installDir} ${configuration}
